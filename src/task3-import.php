440967595203d5cdb55e44eb9853307e728a5138
Fady Fawzy <fady.fawzy19@hotmail.com>
2021-06-21 03:37:57 +0200

    project sync docker snippet

--- src/task3-import.php
+++ src/task3-import.php
@@ -5,3 +5,13 @@ include 'services/importService.php';
 
 $importService = new ImportService();
 print_r($importService->checkImportSinkOperationStatusWithId('ff-testSink','e350c952-38ad-4ded-b100-ae886a54f172'));
+
+//https://github.com/commercetools/commercetools-project-sync#run
+// docker run \
+// -e SOURCE_PROJECT_KEY= \
+// -e SOURCE_CLIENT_ID= \
+// -e SOURCE_CLIENT_SECRET= \
+// -e TARGET_PROJECT_KEY= \
+// -e TARGET_CLIENT_ID= \
+// -e TARGET_CLIENT_SECRET= \
+// commercetools/commercetools-project-sync:3.10.1 -s all
